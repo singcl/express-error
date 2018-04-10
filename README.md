2aa901016534ff31b748c09e0ac045f4312d3b4c
singcl <24661881@qq.com>
2018-04-11 01:12:48 +0800

    :tada: @singcl/express error middleware init.

---
+++ README.md
@@ -0,0 +1,3 @@
+### @singcl/express middleware handle error
+### usage
+`npm i @singcl/express-error -S`
---
+++ index.js
@@ -0,0 +1,22 @@
+// 错误中间件 - 自定义如何处理错误
+module.exports = function(callback) {
+    return function(err, req, res, next) {
+        if (typeof next !== 'function') {
+            next = res
+            res = req
+            req = err
+            err = null
+        }
+
+        if (!err) return next()
+        if (typeof callback === 'function') {
+            callback(err, req, res, next)
+        } else {
+            res.writeHead(500, {'Content-Type': 'text/plain;charset=utf-8'})
+            res.write(err)
+            res.end(function() {
+                console.log('Middleware error [default] | 错误【 ' + err + ' 】已经成功处理！' )
+            })
+        }
+    }
+}
